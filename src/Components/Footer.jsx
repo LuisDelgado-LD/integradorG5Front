ca97ec892e05e19fc0a084d20c4561f61621dc96
yeraldyv <yeraldy.villamizar@javeriana.edu.co>
2025-02-19 21:09:19 -0500

    Se añaden redes socailes e informacion de idioma y moneda en el footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,12 +3,22 @@ const Footer = () => {
     <footer className="footer">
         <div className="footer-CopyRight">
             <img src="/img/ImagenFooter.png" alt="Footer Logo" />
-            <small>Copyright © 2025 Pet Paradise S.A</small>
+            <small>Copyright © 2025 Pet Paradise S.A.</small>
+            <small>Todos los derechos reservados.</small>
         </div>
         <div className="RedesSociales">
-            <img src="/img/Facebook.png" alt="Facebook Logo" />
-            <img src="/img/Instagram.png" alt="Instagram Logo" />
-            <img src="/img/X.png" alt="X Logo" />
+            <small>Español</small>
+            <small>$ COP</small>
+
+            <a href="https://www.facebook.com" target="_blank" title="Visita nuestra página de Facebook">
+                <img src="/img/Facebook.png" alt="Facebook Logo"/>
+            </a>
+            <a href="https://www.Instagram.com" target="_blank" title="Visita nuestro Instagram">
+                <img src="/img/Instagram.png" alt="Instagram Logo" />
+            </a>
+            <a href="https://www.X.com" target="_blank" title="Visita nuestra página en X">
+                <img src="/img/X.png" alt="X Logo" />
+            </a>
         </div>
     </footer>
     )

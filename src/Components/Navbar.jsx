9d214502529ec7283ae4a094c07dabac0656d288
Luis Delgado <luis.delgado13@inacapmail.cl>
2025-03-12 08:31:34 -0300

    añadido calendario

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,28 @@
 import { useLocation } from "react-router-dom";
+import React, { useState, forwardRef  } from "react";
+import DatePicker, {registerLocale} from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import es from "date-fns/locale/es"; // Importa el locale español
+
 
 const Navbar = () => {
+  registerLocale("es", es);
   const location = useLocation();
   if (location.pathname !== "/") return null;
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
+  const onChangeStart = (date) => {
+    // const [start, end] = dates;
+    setStartDate(date);
+    setEndDate(null);
+  };
+  const onChangeEnd = (dates) => {
+    const [start, end] = dates;
+    // setStartDate(start);
+    setEndDate(end);
+  };
 
   return (
     <nav className="navbar">
@@ -12,16 +32,44 @@ const Navbar = () => {
         <div className="date-picker">
           <label className="date-label">Fecha de ingreso</label>
           <div className="calendar-box">
-            <img src="/img/Calendario.png" alt="Calendario ingreso" className="calendar-icon" />
-            <input type="date" className="date-input" />
+          <img src="/img/Calendario.png" alt="Calendario ingreso" className="calendar-icon" />
+            {/* <DatePicker
+              dateFormat="dd/MM/yyyy"
+              //excludeDates={[addDays(new Date(), 1), addDays(new Date(), 5)]}
+              minDate={new Date()}
+              locale={es}
+              selected={startDate}
+              onChange={onChangeStart}
+              Date={startDate}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+            /> */}
+            <DatePicker 
+              withPortal
+              minDate={new Date()}
+              dateFormat="dd/MM/yyyy"
+              selected={startDate} 
+              locale={es}
+              onChange={onChangeStart}
+              />
           </div>
         </div>
-
         <div className="date-picker">
           <label className="date-label">Fecha salida</label>
           <div className="calendar-box">
-            <img src="/img/Calendario.png" alt="Calendario salida" className="calendar-icon" />
-            <input type="date" className="date-input" />
+          <img src="/img/Calendario.png" alt="Calendario ingreso" className="calendar-icon" />
+            <DatePicker
+              withPortal
+              dateFormat="dd/MM/yyyy"
+              locale={es}
+              minDate={startDate}
+              selected={endDate}
+              onChange={onChangeEnd}
+              startDate={startDate}
+              endDate={endDate}
+              selectsRange
+            />
           </div>
         </div>
       </div>

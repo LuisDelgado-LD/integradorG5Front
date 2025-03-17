8a43db12bc142567f97cae7b8703799c4da30006
Luis Delgado <luis.delgado13@inacapmail.cl>
2025-03-17 20:11:33 -0300

    Fix carga de habitaciones en la primera pagina

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -18,90 +18,33 @@ const Home = () => {
   const { categorias } = state;
   const { API_URL } = state;
   const {totalHabitaciones} = state;
-  useEffect(() => {
-    // const pequenas = [
-    //   "Chihuahua", "Pomerania", "Yorkshire", "Pinscher", "Maltés",
-    //   "Pekinés", "Papillón", "Shih Tzu", "Bichón Frisé", "Toy Poodle"
-    // ];
-    // const medianas = [
-    //   "Beagle", "Cocker", "Border Collie", "Bulldog Francés", "Schnauzer",
-    //   "Staffordshire Bull Terrier", "Basenji", "Whippet", "Shetland Sheepdog",
-    //   "American Eskimo", "Shiba Inu", "Corgi", "Springer Spaniel", "Australian Shepherd", "Samoyedo"
-    // ];
-    // const grandes = [
-    //   "Labrador", "Golden Retriever", "Pastor Alemán", "Husky Siberiano", "Rottweiler"
-    // ];
-
-    // let todas = [];
-
-    // pequenas.forEach((nombre, i) => {
-    //   todas.push({
-    //     id: i + 1,
-    //     nombre,
-    //     imagen: "/img/PalacioPeludo.png",
-    //     descripcion: "Espacio acogedor ideal para razas pequeñas.",
-    //     categoria: "Básico",
-    //     tipo: "Pequeña"
-    //   });
-    // });
-
-    // medianas.forEach((nombre, i) => {
-    //   todas.push({
-    //     id: i + 11,
-    //     nombre,
-    //     imagen: "/img/RefugioConfortable.png",
-    //     descripcion: "Ideal para razas medianas con energía y curiosidad.",
-    //     categoria: "Premium",
-    //     tipo: "Mediana"
-    //   });
-    // });
-
-    // grandes.forEach((nombre, i) => {
-    //   todas.push({
-    //     id: i + 26,
-    //     nombre,
-    //     imagen: "/img/CuevaAcogedora.png",
-    //     descripcion: "Amplio y cómodo para razas grandes.",
-    //     categoria: "VIP",
-    //     tipo: "Grande"
-    //   });
-    // });
-
-    // localStorage.setItem("habitacionesMock", JSON.stringify(todas));
-    // setHabitaciones(mezclar(todas));
-    // const habitacionesCache = {}
-    axios.get(API_URL + "/habitaciones/all")
-    .then(response =>{
-      // console.log("habitaciones:", response.data.totalElements);
-      dispatch({ type: "SET_TOTAL_HABITACIONES", payload: response.data.totalElements });
-      const total =[];
-      const habitacionesCache = response.data.content.map(element => {
-        return {
-          id: element.id,
-          nombre: element.nombre,
-          imagen: "/img/PalacioPeludo.png",  // Puedes modificar según lo que necesites
-          descripcion: element.descripcion,
-          categoria: element.categoria.nombre,  // Suponiendo que la categoría tiene un campo 'nombre'
-          tipo: element.tamano
-        };
-      });
-      setHabitaciones(mezclar(habitacionesCache));
-      dispatch({ type: "SET_HABITACIONES", payload: habitacionesCache });
-
-    })
-    // dispatch({ type: "SET_HABITACIONES", payload: todas });
-  }, []);
-
   const totalPaginas = Math.ceil(totalHabitaciones / habitacionesPorPagina);
   const inicio = (paginaActual - 1) * habitacionesPorPagina;
   // const habitacionesPagina = habitaciones.slice(inicio, inicio + habitacionesPorPagina);
   const servicios = state.privilegiosAlojamientos || [];
-  // const categorias = [
-  //   { nombre: "Básico", icono: "/img/1patita.png" },
-  //   { nombre: "Premium", icono: "/img/2patitas.png" },
-  //   { nombre: "VIP", icono: "/img/3patitas.png" },
-  // ];
-  useEffect(() => {
+  const consultaBack = (pagina) => {
+    setPaginaActual(pagina);
+    const pageApi = pagina-1;
+      axios.get(`${API_URL}/habitaciones/all?page=${pageApi}&size${habitacionesPorPagina}`)
+      .then(response =>{
+        // console.log("habitaciones:", response.data.totalElements);
+        dispatch({ type: "SET_TOTAL_HABITACIONES", payload: response.data.totalElements });
+        const total =[];
+        const habitacionesCache = response.data.content.map(element => ({
+          id: element.id,
+          nombre: element.nombre,
+          imagen: "/img/PalacioPeludo.png",
+          descripcion: element.descripcion,
+          categoria: element.categoria.nombre,
+          tipo: element.tamano
+        }));
+        setHabitaciones(mezclar(habitacionesCache));
+        dispatch({ type: "SET_HABITACIONES", payload: habitacionesCache });
+  
+      })
+  }
+  useEffect(() => { // obtener categorias
+    consultaBack(paginaActual);
     axios.get(API_URL + "/categorias")
     .then(response =>{
       console.log(response.data);
@@ -115,26 +58,24 @@ const Home = () => {
   // const cambiarPagina = (pagina) => {
   //   setPaginaActual(pagina);
   // };
-  const handleCambiarPagina = (nuevaPagina) => {
-    const pageApi = nuevaPagina-1;
-    axios.get(API_URL + "/habitaciones/all?page=" + pageApi)
-      .then(response => {
-        dispatch({ type: "SET_TOTAL_HABITACIONES", payload: response.data.totalElements });
-        const habitacionesCache = response.data.content.map(element => ({
-          id: element.id,
-          nombre: element.nombre,
-          imagen: "/img/PalacioPeludo.png",
-          descripcion: element.descripcion,
-          categoria: element.categoria.nombre,
-          tipo: element.tamano
-        }));
-        setHabitaciones(mezclar(habitacionesCache));
-        dispatch({ type: "SET_HABITACIONES", payload: habitacionesCache });
-        setPaginaActual(nuevaPagina);
-      })
-      .catch(error => console.log(error));
-  console.log(habitaciones)
-  };
+  // const handleCambiarPagina = (nuevaPagina) => {
+  //     .then(response => {
+  //       dispatch({ type: "SET_TOTAL_HABITACIONES", payload: response.data.totalElements });
+  //       const habitacionesCache = response.data.content.map(element => ({
+  //         id: element.id,
+  //         nombre: element.nombre,
+  //         imagen: "/img/PalacioPeludo.png",
+  //         descripcion: element.descripcion,
+  //         categoria: element.categoria.nombre,
+  //         tipo: element.tamano
+  //       }));
+  //       setHabitaciones(mezclar(habitacionesCache));
+  //       dispatch({ type: "SET_HABITACIONES", payload: habitacionesCache });
+  //       setPaginaActual(nuevaPagina);
+  //     })
+  //     .catch(error => console.log(error));
+  // console.log(habitaciones)
+  // };
 
   return (
     <div className="home">
@@ -160,12 +101,12 @@ const Home = () => {
         </div>
 
         <div className="paginacion-container">
-          <button disabled={paginaActual === 1} onClick={() => handleCambiarPagina(paginaActual - 1)}>Anterior</button>
+          <button disabled={paginaActual === 1} onClick={() => consultaBack(paginaActual - 1)}>Anterior</button>
           {/* <button disabled={paginaActual === 1} onClick={cambiarPagina(paginaActual - 1)}>Anterior</button> */}
           <span style={{ margin: "0 10px" }}>
             Página {paginaActual} de {totalPaginas}
           </span>
-          <button disabled={paginaActual === totalPaginas} onClick={() => handleCambiarPagina(paginaActual + 1)}>Siguiente</button>
+          <button disabled={paginaActual === totalPaginas} onClick={() => consultaBack(paginaActual + 1)}>Siguiente</button>
           {/* <button disabled={paginaActual === 1} onClick={cambiarPagina(paginaActual + 1)}>Siguiente</button> */}
         </div>
       </div>

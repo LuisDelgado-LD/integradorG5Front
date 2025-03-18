2cfa0250f266a7494215c1064d0aba1ef77cee4c
ydielim93 <mileidy93davila@gmail.com>
2025-03-18 17:41:58 -0500

    Fix Home for merge

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -18,90 +18,6 @@ const Home = () => {
   const { categorias } = state;
   const { API_URL } = state;
   const {totalHabitaciones} = state;
-<<<<<<< HEAD
-  useEffect(() => {
-    // const pequenas = [
-    //   "Chihuahua", "Pomerania", "Yorkshire", "Pinscher", "Maltés",
-    //   "Pekinés", "Papillón", "Shih Tzu", "Bichón Frisé", "Toy Poodle"
-    // ];
-    // const medianas = [
-    //   "Beagle", "Cocker", "Border Collie", "Bulldog Francés", "Schnauzer",
-    //   "Staffordshire Bull Terrier", "Basenji", "Whippet", "Shetland Sheepdog",
-    //   "American Eskimo", "Shiba Inu", "Corgi", "Springer Spaniel", "Australian Shepherd", "Samoyedo"
-    // ];
-    // const grandes = [
-    //   "Labrador", "Golden Retriever", "Pastor Alemán", "Husky Siberiano", "Rottweiler"
-    // ];
-
-    // let todas = [];
-
-    // pequenas.forEach((nombre, i) => {
-    //   todas.push({
-    //     id: i + 1,
-    //     nombre,
-    //     imagen: "/img/PalacioPeludo.png",
-    //     descripcion: "Espacio acogedor ideal para razas pequeñas.",
-    //     categoria: "Básico",
-    //     tipo: "Pequeña"
-    //   });
-    // });
-
-    // medianas.forEach((nombre, i) => {
-    //   todas.push({
-    //     id: i + 11,
-    //     nombre,
-    //     imagen: "/img/RefugioConfortable.png",
-    //     descripcion: "Ideal para razas medianas con energía y curiosidad.",
-    //     categoria: "Premium",
-    //     tipo: "Mediana"
-    //   });
-    // });
-
-    // grandes.forEach((nombre, i) => {
-    //   todas.push({
-    //     id: i + 26,
-    //     nombre,
-    //     imagen: "/img/CuevaAcogedora.png",
-    //     descripcion: "Amplio y cómodo para razas grandes.",
-    //     categoria: "VIP",
-    //     tipo: "Grande"
-    //   });
-    // });
-
-    // localStorage.setItem("habitacionesMock", JSON.stringify(todas));
-    // setHabitaciones(mezclar(todas));
-    // const habitacionesCache = {}
-    axios.get(API_URL + "/habitaciones/all")
-    .then(response =>{
-      // console.log("habitaciones:", response.data.totalElements);
-      dispatch({ type: "SET_TOTAL_HABITACIONES", payload: response.data.totalElements });
-      const total =[];
-      const habitacionesCache = response.data.content.map(element => ({
-        id: element.id,
-        nombre: element.nombre,
-        imagen: "/img/PalacioPeludo.png",
-        descripcion: element.descripcion,
-        categoria: element.categoria.nombre,
-        tipo: element.tamano
-      }));
-      setHabitaciones(mezclar(habitacionesCache));
-      dispatch({ type: "SET_HABITACIONES", payload: habitacionesCache });
-
-    })
-    // dispatch({ type: "SET_HABITACIONES", payload: todas });
-  }, []);
-
-  const totalPaginas = Math.ceil(totalHabitaciones / habitacionesPorPagina);
-  const inicio = (paginaActual - 1) * habitacionesPorPagina;
-  // const habitacionesPagina = habitaciones.slice(inicio, inicio + habitacionesPorPagina);
-  const servicios = state.privilegiosAlojamientos || [];
-  // const categorias = [
-  //   { nombre: "Básico", icono: "/img/1patita.png" },
-  //   { nombre: "Premium", icono: "/img/2patitas.png" },
-  //   { nombre: "VIP", icono: "/img/3patitas.png" },
-  // ];
-  useEffect(() => { // obtener categorias
-=======
   const totalPaginas = Math.ceil(totalHabitaciones / habitacionesPorPagina);
   const inicio = (paginaActual - 1) * habitacionesPorPagina;
   // const habitacionesPagina = habitaciones.slice(inicio, inicio + habitacionesPorPagina);
@@ -129,7 +45,6 @@ const Home = () => {
   }
   useEffect(() => { // obtener categorias
     consultaBack(paginaActual);
->>>>>>> d9994b6ba099fe9c9cf5f887e9d08798ba7cc0b9
     axios.get(API_URL + "/categorias")
     .then(response =>{
       console.log(response.data);

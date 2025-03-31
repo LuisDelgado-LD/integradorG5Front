497f07247d88f9db0f15f49cb579415400050c35
Luis4nge1 <luis24.lmx@gmail.com>
2025-03-30 20:49:26 -0500

    Panel administrador - Maestro (Habitacion)

---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,34 @@
+// Pagination.jsx
+import React from "react";
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+  return (
+    <div className="pagination">
+      <button
+        className="pagination-button"
+        disabled={currentPage === 0}
+        onClick={() => onPageChange(currentPage - 1)}
+      >
+        Anterior
+      </button>
+      {Array.from({ length: totalPages }, (_, i) => (
+        <button
+          key={i}
+          className={`pagination-button ${i === currentPage ? "active" : ""}`}
+          onClick={() => onPageChange(i)}
+        >
+          {i + 1}
+        </button>
+      ))}
+      <button
+        className="pagination-button"
+        disabled={currentPage === totalPages - 1}
+        onClick={() => onPageChange(currentPage + 1)}
+      >
+        Siguiente
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/Routes/Administrador.jsx
+++ src/Routes/Administrador.jsx
@@ -1,10 +1,22 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../Context/utils/globalContext";
 import { FaTrash, FaEdit } from "react-icons/fa";
 import { Link, Outlet , useNavigate } from "react-router-dom";
+import ImageSelect from "../Components/ImageSelect";
+import Pagination from "../Components/Pagination";
+import axios from "axios";
 
 const Administrador = () => {
   const { state, dispatch } = useContext(GlobalContext);
+  const { API_URL } = state;
+  const [dataTable, setDataTable] = useState([]);
+  const [dataCategory, setDataCategory] = useState([]);
+  const [dataFeature, setDataFeature] = useState([]);
+  const [imageList, setImageList] = useState([]);
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  
   const [modalOpen, setModalOpen] = useState(false);
   const [maestroEditado, setMaestroEditado] = useState(null);
   const [error, setError] = useState("");
@@ -13,9 +25,10 @@ const Administrador = () => {
     tipoProducto: "",
     descripcion: "",
     categoria: "",
-    imagenPrincipal: null,
-    imagenSecundaria: null,
-    caracteristicas: [],
+    tamano: "",
+    isDisponible: false,
+    precioUnitario: "",
+    imagen: null,
   });
 
   const handleChange = (e) => {
@@ -30,29 +43,41 @@ const Administrador = () => {
     }
   };
 
-  const handleOpenModal = (maestro = null) => {
-    setMaestroEditado(maestro);
-    setModalOpen(true);
-
+  const handleOpenModal = async (maestro = null) => {
     if (maestro) {
-      setFormData({
-        nombre: maestro.nombre,
-        tipoProducto: maestro.tipoProducto,
-        descripcion: maestro.descripcion,
-        categoria: maestro.categoria,
-        imagenPrincipal: maestro.imagenPrincipal || null,
-        imagenSecundaria: maestro.imagenSecundaria || null,
-      });
+      console.log("🆔 ID seleccionado:", maestro.id);
+      try {
+        const response = await axios.get(`https://petparadise.sytes.net/api/habitaciones/${maestro.id}`);
+        const data = response.data;
+
+  
+        setFormData({
+          id: data.id,
+          nombre: data.nombre || "",
+          tipoProducto: data.tipoProducto || "Producto",
+          descripcion: data.descripcion || "",
+          tamano: data.tamano || "",
+          isDisponible: data.isDisponible || "false",
+          precioUnitario: data.precioUnitario || "",
+          categoria: data.categoria.id || "",
+          imagen: null,
+        });
+      } catch (error) {
+        console.error("❌ Error al obtener datos:", error);
+        setError("No se pudieron cargar los datos.");
+      }
     } else {
       setFormData({
+        id: null,
         nombre: "",
         tipoProducto: "",
         descripcion: "",
         categoria: "",
-        imagenPrincipal: null,
-        imagenSecundaria: null,
+        imagen: null,
       });
     }
+    setMaestroEditado(maestro);
+    setModalOpen(true);
   };
 
   const handleCloseModal = () => {
@@ -61,39 +86,159 @@ const Administrador = () => {
     setError("");
   };
 
-  const handleSave = () => {
+  const handleSave = async () => {
+    console.log(formData);
     if (!formData.nombre || !formData.tipoProducto || !formData.descripcion || !formData.categoria) {
       setError("❌ Todos los campos son obligatorios.");
       return;
     }
 
-    if (!formData.imagenPrincipal || !formData.imagenSecundaria) {
-      setError("❌ Debes subir ambas imágenes.");
+    if (!formData.imagen) {
+      setError("❌ Debes subir todas las imágenes.");
       return;
     }
 
-    if (maestroEditado) {
-      dispatch({ type: "EDITAR_MAESTRO", payload: { id: maestroEditado.id, ...formData } });
-    } else {
-      dispatch({
-        type: "AGREGAR_MAESTRO",
-        payload: { id: state.maestros.length + 1, ...formData },
-      });
+    console.log("📤 Datos enviados:", formData);
+    
+    const payload = {
+      nombre: formData.nombre,
+      descripcion: formData.descripcion,
+      imagen: { 
+        url: formData.imagen, 
+        esPrincipal: true 
+      },
+      categoria: { id: parseInt(formData.categoria) },
+      tamano: formData.tamano,
+      isDisponible: formData.isDisponible === "true" || formData.isDisponible === true,
+      precioUnitario: parseFloat(formData.precioUnitario)
+    };
+    console.log("📤 Payload a enviar:", payload);
+    try {
+      let maestroResponse;
+      if (maestroEditado) {
+        // En modo edición, actualizamos directamente (suponiendo que el backend actualiza también imágenes)
+        maestroResponse = await axios.put(`${API_URL}/habitaciones/${maestroEditado.id}`, payload, {
+          headers: { "Content-Type": "application/json" }
+        });
+      } else {
+ 
+        maestroResponse = await axios.post(`${API_URL}/habitaciones`, payload, {
+          headers: { "Content-Type": "application/json" }
+        });
+      }
+
+
+      if (maestroEditado) {
+        dispatch({ type: "EDITAR_MAESTRO", payload: maestroResponse.data });
+      } else {
+        dispatch({ type: "AGREGAR_MAESTRO", payload: maestroResponse.data });
+      }
+
+      handleCloseModal();
+      
+      // Opcional: Actualiza la tabla volviendo a obtener la lista.
+      const tableResponse = await axios.get(`${API_URL}/habitaciones`);
+      const data = tableResponse.data.content.map(hab => ({
+        id: hab.id,
+        nombre: hab.nombre,
+        tipo: 2,
+        fecha: new Date().toISOString().split('T')[0]
+      }));
+      setDataTable(data);
+      setCurrentPage(0);
+    } catch (error) {
+      console.error("❌ Error al guardar:", error);
+      setError("Ocurrió un error al guardar los datos.");
     }
+
+    // if (maestroEditado) {
+    //   dispatch({ type: "EDITAR_MAESTRO", payload: { id: maestroEditado.id, ...formData } });
+    // } else {
+    //   dispatch({
+    //     type: "AGREGAR_MAESTRO",
+    //     payload: { id: state.maestros.length + 1, ...formData },
+    //   });
+    // }
 
     handleCloseModal();
   };
 
-  const handleDelete = (id) => {
-    dispatch({ type: "ELIMINAR_MAESTRO", payload: id });
+  const handleDelete = (id) => { 
+    const confirmDelete = window.confirm("¿Estás seguro de que deseas eliminar este elemento?");
+    if (!confirmDelete) return;
+    
+    axios.delete(`${API_URL}/habitaciones/${id}`)
+    .then((response) => {
+      console.log("Estado producto:", response);
+      setDataTable(prevData => prevData.filter(item => item.id !== id));
+      dispatch({ type: "ELIMINAR_MAESTRO", payload: id });
+    })
+    .catch((error) => console.error("Error al eliminar:", error));
   };
+
+  /*const handleDelete = (id) => {
+    console.log(id);
+    dispatch({ type: "ELIMINAR_MAESTRO", payload: id });
+  };*/
+
+  useEffect(() => {
+      axios.get(`${API_URL}/habitaciones?page=${currentPage}`)
+      .then(response => {
+        const data = response.data.content.map(hab => ({
+          id: hab.id,
+          nombre: hab.nombre,
+          tipo: 2,
+          fecha: new Date().toISOString().split('T')[0] // Fecha actual
+        }));
+        //console.log(data);
+        setDataTable(data);
+        setTotalPages(response.data.totalPages);
+      })
+      .catch(error => console.error("Error al obtener habitaciones:", error));
+  }, [API_URL, currentPage]);
+
+  useEffect(() => {
+    axios.get(`${API_URL}/categorias`)
+    .then(response => {
+      const data = response.data.map(category => ({
+        id: category.id,
+        nombre: category.nombre,
+      }));
+      setDataCategory(data);
+    })
+    .catch(error => console.error("Error al obtener categorias:", error));
+}, []);
+
+useEffect(() => {
+  axios.get(`${API_URL}/imagenes/todas`)
+  .then(response => {
+    const data = response.data.map(image => ({
+      id: image.id,
+      url: image.url,
+    }));
+    setImageList(data);
+  })
+  .catch(error => console.error("Error al obtener las imagenes:", error));
+}, []);
+
+useEffect(() => {
+  axios.get(`${API_URL}/caracteristicas`)
+  .then(response => {
+    const data = response.data.map(feature => ({
+      id: feature.id,
+      nombre: feature.nombre,
+    }));
+    setDataFeature(data);
+  })
+  .catch(error => console.error("Error al obtener caracteristica:", error));
+}, []);
 
   return (
     <div className="admin-container">
       <h2>Panel de Administración</h2>
 
       <nav className="admin-nav">
-        <Link to="/administrador/gestion-caracteristicas">Gestión de Caracteristicas</Link>
+        <Link to="/administrador/gestion-maestro">Gestión de Maestro</Link>
         <Link to="/administrador/gestion-de-usuario">Gestión de Usuarios</Link>
       </nav>
 
@@ -101,24 +246,22 @@ const Administrador = () => {
 
       <h2>Gestión de Maestro</h2>
 
-      {state.maestros.length > 0 ? (
+      {dataTable.length > 0 ? (
         <table className="product-table">
           <thead>
             <tr>
               <th>Nombre</th>
               <th>Tipo de Maestro</th>
-              <th>Categoría</th>
-              <th>Características</th>
+              <th>Fecha de modificación</th>
               <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
-            {state.maestros.map((maestro) => (
+            {dataTable.map((maestro) => (
               <tr key={maestro.id}>
                 <td>{maestro.nombre}</td>
-                <td>{maestro.tipoProducto}</td>
-                <td>{maestro.categoria}</td>
-                <td>{maestro.caracteristicas?.join(", ")}</td>
+                <td>{maestro.tipo == 1 ? "Categoria" : "Producto"}</td>
+                <td>{maestro.fecha}</td>
                 <td>
                   <FaEdit className="edit-icon" onClick={() => handleOpenModal(maestro)} />
                   <FaTrash className="delete-icon" onClick={() => handleDelete(maestro.id)} />
@@ -130,59 +273,129 @@ const Administrador = () => {
       ) : (
         <p>No hay maestros registrados.</p>
       )}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={(page) => setCurrentPage(page)}
+        />
+      )}
 
       <button className="add-user" onClick={() => handleOpenModal()}>➕ Añadir Maestro</button>
 
       {modalOpen && (
         <div className="modal-overlay">
           <div className="modal">
+            <button className="close-modal" onClick={handleCloseModal}>✖</button>
             <h2>{maestroEditado ? "Editar Maestro" : "Nuevo Maestro"}</h2>
             {error && <p className="error-message">{error}</p>}
 
-            <label>Nombre:</label>
-            <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} required />
+            <div className="modal-content">
+              <label>Nombre:</label>
+              <input type="text" name="nombre" value={formData.nombre} onChange={handleChange} required />
 
-            <label>Tipo de Maestro:</label>
-            <input type="text" name="tipoProducto" value={formData.tipoProducto} onChange={handleChange} required />
+              <label>Tipo de Maestro:</label>
+              <input type="text" name="tipoProducto" value={formData.tipoProducto} onChange={handleChange} required />
 
-            <label>Categoría:</label>
-            <select name="categoria" value={formData.categoria} onChange={handleChange} required>
-              <option value="">Seleccione una categoría</option>
-              {state.categorias.map((cat, index) => (
-                <option key={index} value={cat}>{cat}</option>
-              ))}
-            </select>
-
-            <label>Características:</label>
-            <select
-              name="caracteristicas"
-              multiple
-              value={formData.caracteristicas || []}
-              onChange={(e) =>
-                setFormData({
-                ...formData,
-                caracteristicas: Array.from(e.target.selectedOptions, (option) => option.value),
-                })
-                }
->
-                {state.caracteristicas.map((car, index) => (
-                <option key={index} value={car.nombre}>
-                {car.nombre}
-                </option>
+              <label>Categoría:</label>
+              <select name="categoria" value={formData.categoria} onChange={handleChange} required>
+                <option value="">Seleccione una categoría</option>
+                {dataCategory.map((cat, index) => (
+                  <option key={index} value={cat.id}>{cat.nombre}</option>
                 ))}
-            
-            </select>
+              </select>
 
-            <label>Descripción:</label>
-            <textarea name="descripcion" value={formData.descripcion} onChange={handleChange} required />
+              <label>Tamaño:</label>
+              <select
+                name="tamano"
+                value={formData.tamano || ""}
+                onChange={handleChange}
+                required
+              >
+                <option value="">Seleccione un tamaño</option>
+                <option value="GRANDE">Grande</option>
+                <option value="MEDIANA">Mediano</option>
+                <option value="PEQUENA">Pequeño</option>
+              </select>
 
-            <label>Imagen Principal:</label>
-            <input type="file" name="imagenPrincipal" accept="image/*" onChange={handleImageChange} />
-            {formData.imagenPrincipal && <img src={formData.imagenPrincipal} alt="Imagen Principal" width="100" />}
+              <label>Disponible:</label>
+              <select
+                name="isDisponible"
+                value={formData.isDisponible !== undefined ? formData.isDisponible.toString() : ""}
+                onChange={(e) =>
+                  setFormData({
+                    ...formData,
+                    isDisponible: e.target.value === "true",
+                  })
+                }
+                required
+              >
+                <option value="">Seleccione disponibilidad</option>
+                <option value="true">Disponible</option>
+                <option value="false">No Disponible</option>
+              </select>
 
-            <label>Imagen Secundaria:</label>
-            <input type="file" name="imagenSecundaria" accept="image/*" onChange={handleImageChange} />
-            {formData.imagenSecundaria && <img src={formData.imagenSecundaria} alt="Imagen Secundaria" width="100" />}
+              <label>Precio Unitario:</label>
+              <input
+                type="number"
+                name="precioUnitario"
+                value={formData.precioUnitario || ""}
+                onChange={handleChange}
+                required
+              />
+
+              <label htmlFor="descripcion">Descripción:</label>
+              <textarea
+                id="descripcion"
+                name="descripcion"
+                value={formData.descripcion || ''}
+                onChange={handleChange}
+                required
+              ></textarea>
+
+              {/* <label>Características:</label>
+              <select
+                name="caracteristicas"
+                multiple
+                value={formData.caracteristicas || []}
+                onChange={(e) =>
+                  setFormData({
+                  ...formData,
+                  caracteristicas: Array.from(e.target.selectedOptions, (option) => option.value),
+                  })
+                  }
+                  className="multi-select">
+                  {dataFeature?.length > 0 ? (
+                    dataFeature.map((car, index) => (
+                      <option key={index} value={car.id}>
+                        {car.nombre}
+                      </option>
+                    ))
+                  ) : (
+                    <option disabled>Cargando opciones...</option>
+                  )}
+              
+              </select> */}
+
+              <label>Imagen Principal:</label>
+              <ImageSelect
+                options={imageList}
+                value={formData.imagenId} // Guarda el id seleccionado
+                onChange={(selected) =>
+                  setFormData((prev) => ({ ...prev, imagenId: selected.value, imagen: selected.url }))
+                }
+                menuPortalTarget={document.body}  // Renderiza el menú en un portal
+                styles={{
+                  menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+                  menu: (base) => ({
+                    ...base,
+                    maxHeight: '300px', // Limita la altura
+                    overflowY: 'auto',
+                  }),
+                }}
+              />
+              {formData.imagen && <img src={formData.imagen} alt="Imagen seleccionada" className="preview-img" />}
+            </div>
 
             <div className="modal-buttons">
               <button onClick={handleSave} className="save-button">Guardar</button>

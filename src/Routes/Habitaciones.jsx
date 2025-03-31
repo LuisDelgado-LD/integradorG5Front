1c5a77f67e2aafc985c1319f3c7f768a6a9edc26
yeraldyv <yeraldy.villamizar@javeriana.edu.co>
2025-03-30 21:30:28 -0500

    Se ajustan estilos en las habitaciones por modificacion del codigo

--- src/Routes/Habitaciones.jsx
+++ src/Routes/Habitaciones.jsx
@@ -90,36 +90,42 @@ const Habitaciones = () => {
   if (!habitacion) return <p>Cargando habitación...</p>;
 
   return (
-    <div className="habitacion-container" style={{ padding: "20px" }}>
-      <h2>{habitacion.nombre}</h2>
-      <button className="back-home" onClick={() => navigate("/")}>
-        <img src="/img/flecha.png" alt="Volver" />
-      </button>
-
-      <div>
-        <img className="habitacion-img" src={habitacion.imagen} alt={habitacion.nombre} style={{ width: "100%" }} />
-        <button className="ver-mas" onClick={() => navigate(`/galeria/${habitacion.id}`)}>Ver más</button>
+    <div className="habitacion-container-Principal" style={{ padding: "20px" }}>
+      <div className="Encabezado">
+        <h2>{habitacion.nombre}</h2>
+        <button className="back-home" onClick={() => navigate("/")}>
+          <img src="/img/flecha.png" alt="Volver" />
+        </button>
       </div>
+      <div className="habitacion-container">
+        <div className="habitacion-img-container">
+          <img className="habitacion-img" src={habitacion.imagen} alt={habitacion.nombre} style={{ width: "100%" }} />
+        </div>
+        
+        <div className="habitacion-content">
+          <p><strong>Categoría:</strong> {habitacion.categoria}</p>
+          <p><strong>Descripción:</strong> {descripcionesCategoria[habitacion.categoria]}</p>
+        </div>
+      </div>
+      <button className="ver-mas" onClick={() => navigate(`/galeria/${habitacion.id}`)}>Ver más</button>
 
-      <p><strong>Categoría:</strong> {habitacion.categoria}</p>
-      <p><strong>Descripción:</strong> {descripcionesCategoria[habitacion.categoria]}</p>
+      <div className="habitacion-container2">
+        <p><strong>Características:</strong></p>
+        <ul>
+          {caracteristicasPorCategoria[habitacion.categoria].map((car, index) => (
+            <li key={index}>
+              <img src={iconosCaracteristicas[car]} alt={car} width="24" /> {car}
+            </li>
+          ))}
+        </ul>
 
-      <p><strong>Características:</strong></p>
-      <ul>
-        {caracteristicasPorCategoria[habitacion.categoria].map((car, index) => (
-          <li key={index}>
-            <img src={iconosCaracteristicas[car]} alt={car} width="24" /> {car}
-          </li>
-        ))}
-      </ul>
-
-      <p><strong>Selecciona tu rango de fechas:</strong></p>
-      <DatePicker selected={fechaInicio} onChange={setFechaInicio} placeholderText="Fecha de inicio" />
-      <DatePicker selected={fechaFin} onChange={setFechaFin} placeholderText="Fecha de fin" />
-
+        <p><strong>Selecciona tu rango de fechas:</strong></p>
+        <DatePicker selected={fechaInicio} onChange={setFechaInicio} placeholderText="Fecha de inicio" />
+        <DatePicker selected={fechaFin} onChange={setFechaFin} placeholderText="Fecha de fin" />
+      </div>
       <button onClick={reservar} disabled={!fechaInicio || !fechaFin} style={{ backgroundColor: "#30384D", color: "white" }}>
-        Reservar ahora
-      </button>
+          Reservar ahora
+        </button>
     </div>
   );
 };
